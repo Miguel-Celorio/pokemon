a91a0ba66f398e6b120ac859721f793bd79fd5a7
Miguel <Miguel@192.168.1.131>
2022-01-09 20:17:24 +0100

    Class battle

--- pokemon/src/pokemon/Battle.java
+++ pokemon/src/pokemon/Battle.java
@@ -2,8 +2,40 @@ package pokemon;
 
 public class Battle {
 	public static void initBattle(Pokemon pokemon1, Pokemon pokemon2) {
+		
+		Pokemon primerPokemon = pokemon1;
+		Pokemon segundoPokemon = pokemon2;
+
 		while(pokemon1.getHealth() > 0 && pokemon2.getHealth() > 0) {
+			pokemon2.setHealth(pokemon2.getHealth() -pokemon1.getStrength());
+			pokemon1.setHealth(pokemon1.getHealth() -pokemon2.getStrength());
+			
+			if(pokemon1.getSpeed() > pokemon2.getSpeed()) {
+				primerPokemon = pokemon1;
+				segundoPokemon = pokemon2;
+			} else {
+				primerPokemon = pokemon2;
+				segundoPokemon = pokemon1;
+			}
+			
+			System.out.println(primerPokemon.getName() + " comienza el ataque contra " + segundoPokemon.getName());
+			
+			System.out.println(primerPokemon.getName() + " daña " + primerPokemon.getStrength() + " a " + 
+			segundoPokemon.getName() + " y a " + segundoPokemon.getName() + " le queda " + 
+			segundoPokemon.getHealth() + " vida.");
+			System.out.println(segundoPokemon.getName() + " daña " + segundoPokemon.getStrength() + " a " + 
+					primerPokemon.getName() + " y a " + primerPokemon.getName() + " le queda " + 
+					primerPokemon.getHealth() + " vida.");
 			
 		}
+		
+		System.out.println("-------------------------------------------");
+		if(pokemon1.getHealth() <= 0) {
+			System.out.println(pokemon1.getName() + " ha perdido el combate");
+		} else {
+			System.out.println(pokemon2.getName() + " ha perdido el combate");
+		}
+		
+		
 	}
 }
